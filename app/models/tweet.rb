35b24ad45d8e0b97f3bcf0fabc654a521854da7c
Sasho1989 <caki.jova@gmail.com>
2020-07-04 13:35:41 +0200

    Association between Tweet and Comment models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
+  has_many :comments
+
   validates :username, presence: true, length: { minimum: 4, maximum: 25 }
   validates :body, presence: true, length: { maximum: 140 }
 end
