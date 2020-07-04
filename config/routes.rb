92891aec81d67208349495ebf2e54c9ef900878f
Sasho1989 <caki.jova@gmail.com>
2020-07-04 14:05:02 +0200

    Displaying tweet comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   #get 'tweets/index'
 
-  resources :tweets
-
   root to: 'tweets#index'
+
+  resources :tweets do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
